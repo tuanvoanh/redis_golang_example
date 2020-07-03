2cf9c591a53ada7d7c691addc6cf32dcd0e67034
TuanVo <94voanhtuan@gmail.com>
2020-07-03 21:46:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+var ctx = context.Background()
+
+type Author struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+	fmt.Println("Go Redis Tutorial")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping(ctx).Result()
+	fmt.Println(pong, err)
+	json, err := json.Marshal(Author{Name: "Elliot", Age: 25})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = client.Set(ctx, "id1234", json, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	val, err := client.Get(ctx, "id1234").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(val)
+}
